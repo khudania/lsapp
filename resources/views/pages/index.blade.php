c6e809fd70ffc4f4ae5229dc6b04504754c7ee3a
Himanshu Khudania <hkhudania@gmail.com>
2020-07-14 21:27:56 +0530

    updated layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-        <h1>Welcome to My project</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem excepturi voluptatum hic sed enim mollitia minima, nobis et quod minus.</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<h1>Welcome to My project..</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem excepturi voluptatum hic sed enim mollitia minima, nobis et quod minus.</p>
+@endsection  
