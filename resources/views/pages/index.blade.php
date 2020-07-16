094ab557af7d972530c56aaa4aff4f4eae59f3c0
Himanshu Khudania <hkhudania@gmail.com>
2020-07-16 21:02:57 +0530

    added items to Homepage

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{$title}}</h1>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem excepturi voluptatum hic sed enim mollitia minima, nobis et quod minus.</p>
+<div class="jumbotron text-center bg-light p-4">
+    <h1>Welcome To LSAPP</h1>
+    <p>This is My First laravel Project</p>
+    <p>
+        <a href="/login" class="btn btn-primary btn-lg" role="button">Login</a>
+        <a href="/register" class="btn btn-success btn-lg" role="button">Register</a>
+    </p>
+</div>
 @endsection  
